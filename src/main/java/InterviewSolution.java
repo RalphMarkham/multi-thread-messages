1e9b375f96cf9819425e240d9d8ea9b5b6b95c80
Ralph Markham <rem@rogers.com>
2022-05-07 01:53:30 -0400

    reconsidered

--- src/main/java/InterviewSolution.java
+++ src/main/java/InterviewSolution.java
@@ -211,20 +211,6 @@ public class InterviewSolution {
                     }
                 } while(!this.isEmpty());
             }
-
-            @Override
-            public boolean equals(Object o) {
-                if (this == o) return true;
-                if (o == null || getClass() != o.getClass()) return false;
-                if (!super.equals(o)) return false;
-                TaskQueue taskQueue = (TaskQueue) o;
-                return key.equals(taskQueue.key);
-            }
-
-            @Override
-            public int hashCode() {
-                return Objects.hash(super.hashCode(), key);
-            }
         }
 
         // A lock to synchronize when mappedTasks and it value objects
