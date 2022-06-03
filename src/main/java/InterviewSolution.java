7c39d76dbf1bd0feab4c1fe7f121eeae9092ee22
Ralph Markham <rem@rogers.com>
2022-06-02 22:14:19 -0400

    refining a pattern

--- src/main/java/InterviewSolution.java
+++ src/main/java/InterviewSolution.java
@@ -3,29 +3,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Queue;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class InterviewSolution {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
     private static void run(int threads, String path) {
-        FixedOrderedExecutor<String> es = new FixedOrderedExecutor<>(threads);
+        BiFunctionFixedOrderedExecutor<String> func = new BiFunctionFixedOrderedExecutor<>(threads);
 
         try {
             @SuppressWarnings("rawtypes") CompletableFuture[] futures = Files.lines(Paths.get(path))
@@ -53,7 +37,7 @@ public class InterviewSolution {
                             // Because FixedOrderedExecutor / es, maintains the key Runnable Queue pairs,
                             // which are updated by es and one of its inner classes, just using
                             // CompletableFuture.runAsync() becomes a little more impractical in this stream.
-                            cf = es.submit(l.substring(0,firstIndx), new Consumer(l, firstIndx, lastIndx));
+                            cf = func.apply(() -> l.substring(0,firstIndx), new Consumer(l, firstIndx, lastIndx));
                         }
                     }
                     return cf;
@@ -65,7 +49,7 @@ public class InterviewSolution {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            es.shutdown();
+            func.shutdown();
         }
     }
 
@@ -116,200 +100,6 @@ public class InterviewSolution {
 
             String end = DATE_TIME_FORMATTER.format(LocalTime.now());
             System.out.println(line+";  \tThread: "+threadId+";\tStart: "+start+";\tEnd: "+end);
-        }
-    }
-
-    /**
-     * All messages with the same id must be processed in the order they appear in the file.
-     *
-     * A simple delegate ExecutorService, extended to support the execution
-     * of key Runnable pairs, ensuring sequential execution based on key.
-     * Key based sequential execution is managed by maintaining a map of key
-     * Runnable Queue pairs. Upon completion the Runnable Queue inner class
-     * removes itself from the map.
-     *
-     * @param <K> the key type
-     */
-    private static class FixedOrderedExecutor<K> implements ExecutorService {
-
-        /**
-         *  A simpler version of AsyncRun from CompletableFuture, used to
-         *  couple the Runnable and CompletableFuture inorder to provide a
-         *  more elegant solution for maintaining a connection to the Runnable
-         *  after it has been submitted.
-         */
-        private static class RunnableCompletableFuture implements Runnable {
-            private final Runnable r;
-            private final CompletableFuture<Void> cf;
-
-            public RunnableCompletableFuture(Runnable r, CompletableFuture<Void> cf) {
-                this.r = r;
-                this.cf = cf;
-            }
-
-            @Override
-            public void run() {
-                if (r != null && cf != null) {
-                    try {
-                        r.run();
-                        cf.complete(null);
-                    } catch (Throwable ex) {
-                        cf.obtrudeException(ex);
-                    }
-                }
-            }
-        }
-
-        /**
-         * Wrapper ensuring the sequential execution of key Runnable pairs.
-         *
-         * Under normal circumstances an I would classify this kind inheritance
-         * as an anti-pattern, much better served by composition, as inheritance
-         * does not provide any encapsulation to its super classes, but as a
-         * private inner class, it provides both a simple and effective data structure
-         * encapsulated in the FixedOrderedExecutor class.
-         */
-        private class TaskQueue extends LinkedList<Runnable> implements Runnable {
-            private final K key;
-
-            public TaskQueue(K key, Runnable runnable) {
-                super();
-
-                Objects.requireNonNull(runnable, "Everybody knows a Runnable can not be null, please rethink your options");
-
-                this.key = key;
-                this.push(runnable);
-            }
-
-            @Override
-            public void run() {
-                // Under normal circumstances, the potential for throwing
-                // a NullPointerException would exclude the use of do while.
-                // But because we know our trusty constructor is verifying
-                // a non-null runnable, do while is a safe bet.
-                //
-                // Do while we still have a Runnable to run
-                do {
-                    // Retrieve, remove, and run the head of the list
-                    //noinspection ConstantConditions
-                    this.poll().run();
-
-                    // Optimize use of lock, as running this out side of the
-                    // loop runs the chance that a runnable is added before
-                    // this is remove from mappedTasks.
-                    if (this.size() < 1) {
-                        // Make sure we still have a Runnable to run,
-                        // otherwise remove this list from mappedTasks
-                        try {
-                            lock.lock();
-                            if (this.isEmpty()) {
-                                mappedTasks.remove(key);
-                            }
-                        } finally {
-                            lock.unlock();
-                        }
-                    }
-                } while(!this.isEmpty());
-            }
-        }
-
-        // A lock to synchronize when mappedTasks and it value objects
-        private final Lock lock = new ReentrantLock();
-
-
-        // Sequential execution of key based lists requires fail fast concurrent behaviour
-        private final Map<K, Queue<Runnable>> mappedTasks = new HashMap<>();
-        private final ExecutorService delegate;
-
-        public FixedOrderedExecutor(int threads) {
-            delegate = Executors.newFixedThreadPool(threads);
-        }
-
-        public CompletableFuture<Void> submit(K key, Runnable runnable) {
-            // Wrap the runnable with a CompletableFuture
-            RunnableCompletableFuture run = new RunnableCompletableFuture(runnable, new CompletableFuture<>());
-            try {
-                lock.lock();
-                // Search for an existing Queue of Runnable if found, add it to the Queue, otherwise
-                // put the key and a new Runnable Queue, containing run into mappedTasks
-                Queue<Runnable> queue = mappedTasks.get(key);
-                if (queue != null) {
-                    queue.add(run);
-                } else {
-                    TaskQueue tq = new TaskQueue(key, run);
-                    mappedTasks.put(key, tq);
-                    delegate.execute(tq);
-                }
-            } finally {
-                lock.unlock();
-            }
-
-            return run.cf;
-        }
-
-        @Override
-        public void shutdown() {
-            delegate.shutdown();
-        }
-
-        @Override
-        public List<Runnable> shutdownNow() {
-            return delegate.shutdownNow();
-        }
-
-        @Override
-        public boolean isShutdown() {
-            return delegate.isShutdown();
-        }
-
-        @Override
-        public boolean isTerminated() {
-            return delegate.isTerminated();
-        }
-
-        @Override
-        public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-            return delegate.awaitTermination(timeout, unit);
-        }
-
-        @Override
-        public <T> Future<T> submit(Callable<T> task) {
-            return delegate.submit(task);
-        }
-
-        @Override
-        public <T> Future<T> submit(Runnable task, T result) {
-            return delegate.submit(task, result);
-        }
-
-        @Override
-        public Future<?> submit(Runnable task) {
-            return delegate.submit(task);
-        }
-
-        @Override
-        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-            return delegate.invokeAll(tasks);
-        }
-
-        @Override
-        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-            return delegate.invokeAll(tasks, timeout, unit);
-        }
-
-        @Override
-        public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-            return delegate.invokeAny(tasks);
-        }
-
-        @Override
-        public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-            return delegate.invokeAny(tasks, timeout, unit);
-        }
-
-        @Override
-        public void execute(Runnable command) {
-            delegate.execute(command);
         }
     }
 }
